ef3e9e901c7bc02d4daf80e0a309d14a7f282b0e
evandro.alvatar@gmail.com <evandro.alvatar@gmail.com@82bbdaba-fbe1-b293-0f69-b142ebb6a897>
2012-10-02 03:50:15 +0000

    Classe "Canvas" renomeada e atualizada para "CanvasBase". Classe "AnimationDrawableTest" tem exemplo de animação...

---
+++ trunk/src/canvas/CanvasBase.java
@@ -0,0 +1,79 @@
+package canvas;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+// keyboard information:
+// http://code.google.com/p/libgdx/wiki/InputEvent
+// http://libgdx.l33tlabs.org/docs/api/com/badlogic/gdx/Input.Keys.html
+public class CanvasBase implements InputProcessor{
+	protected CanvasController controller;
+	
+	public CanvasBase(CanvasController controller){
+		this.controller = controller;
+	}
+	
+	public void render(SpriteBatch spriteBatch) {
+
+	}
+	
+	public void update(float deltaTime) {
+
+	}
+	
+	public void resize(int width, int height) {
+
+	}
+	
+	public void pause() {
+
+	}
+	
+	public void resume() {
+
+	}
+	
+	public void dispose() {
+
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+	
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+	
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+	
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+	
+	@Override
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		return false;
+	}
+	
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		return false;
+	}
+	
+	@Override
+	public boolean touchMoved(int x, int y) {
+		return false;
+	}
+	
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		return false;
+	}
+}
---
+++ trunk/src/character/AnimationDrawableTest.java
@@ -0,0 +1,68 @@
+package character;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AnimationDrawableTest{
+	protected Texture texture;
+	protected TextureRegion[] textures;
+	protected Animation animation;
+	protected TextureRegion currentFrame;
+	
+	protected int animationFramesColumns;
+	protected int animationFramesRows;
+	
+	protected float stateTime;
+	
+	protected int x;
+	protected int y;
+	
+	protected int speedX;
+	protected int speedY;
+	
+	public AnimationDrawableTest(String imagePath){
+		animationFramesColumns = 4;
+		animationFramesRows = 4;
+		
+		texture = new Texture(Gdx.files.internal(imagePath));
+		TextureRegion[][] splitedTextures = TextureRegion.split(texture, texture.getWidth() / animationFramesColumns, texture.getHeight() / animationFramesRows);
+		textures = new TextureRegion[animationFramesColumns * animationFramesRows];
+		int index = 0;
+		for(int i = 0; i < animationFramesRows; i++){
+			for(int j = 0; j < animationFramesColumns; j++){
+				textures[index++] = splitedTextures[i][j];
+			}
+		}
+		
+		animation = new Animation(0.025f, textures);
+		stateTime = 0f;
+		
+		x = 0;
+		y = 0;
+		
+		speedX = 100;
+		speedY = 100;
+	}
+	
+	public void render(SpriteBatch spriteBatch) {
+		stateTime += Gdx.graphics.getDeltaTime();
+		currentFrame = animation.getKeyFrame(stateTime, true);
+		spriteBatch.draw(currentFrame, x, y);
+	}
+	
+	public void update() {
+		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT))
+			x -= Gdx.graphics.getDeltaTime() * speedX;
+		if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT))
+			x += Gdx.graphics.getDeltaTime() * speedX;
+		if(Gdx.input.isKeyPressed(Keys.DPAD_UP))
+			y += Gdx.graphics.getDeltaTime() * speedY;
+		if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN))
+			y -= Gdx.graphics.getDeltaTime() * speedY;
+		
+	}
+}

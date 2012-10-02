058436fcc052e01655bdc34a03689620fa0a25a5
evandro.alvatar@gmail.com <evandro.alvatar@gmail.com@82bbdaba-fbe1-b293-0f69-b142ebb6a897>
2012-10-02 03:45:52 +0000

    Removido package drawable. Criado input, criado troca de telas, pressionando "enter", criado exemplos de audio, musicas e imagens, na classe CanvasGame.

--- trunk/src/canvas/CanvasSplash.java
+++ trunk/src/canvas/CanvasSplash.java
@@ -1,93 +1,21 @@
 package canvas;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 
-import core.Application;
-
-public class CanvasSplash extends Canvas{
+public class CanvasSplash extends CanvasBase{
 	
-	public CanvasSplash(Application application){
-		super(application);
-		
-	}
-	
-	public void render(SpriteBatch spriteBatch) {
-
+	public CanvasSplash(CanvasController controller){
+		super(controller);
+		System.out.println("CanvasSplash");
 	}
 	
-	public void update(float deltaTime) {
-
-	}
-
 	@Override
-	public boolean keyDown(int arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchMoved(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	public boolean keyTyped(char character) {
+		if(Gdx.input.isKeyPressed(Keys.ENTER)){
+			System.out.println("CanvasSplash Enter");
+			controller.setCanvasByName(CanvasController.CANVAS_MENU);
+		}
+		return super.keyTyped(character);
 	}
 }

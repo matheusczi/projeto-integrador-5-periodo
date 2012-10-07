c35818e8cb992660f6623b311279e2d77ea87a8f
guilhermopazuch@gmail.com <guilhermopazuch@gmail.com@82bbdaba-fbe1-b293-0f69-b142ebb6a897>
2012-10-07 15:48:17 +0000

    levelparser and commonresources

--- src/level/Cell.java
+++ src/level/Cell.java
@@ -1,16 +1,23 @@
 package level;
 
+import utils.CommonResources;
+
+import com.badlogic.gdx.graphics.Texture;
+
 public class Cell {
 	public String tile;
 	public boolean hasBlock;
 	public boolean isBlockTarget;
 	public boolean isWalkable;
+	public Texture texture;
 	
 	public Cell(String tile, boolean hasBlock, boolean isBlockTarget, boolean isWalkable){
 		this.tile = tile;
+		this.texture = CommonResources.getInstance().assets.get(tile);
 		this.hasBlock = hasBlock;
 		this.isBlockTarget = isBlockTarget;
 		this.isWalkable = isWalkable;
+		this.texture = texture;
 	}
 	
 	public Cell(){

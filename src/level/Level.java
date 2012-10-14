43a3f5ee843a1d390bf841df7473c1f25d31f19b
guilhermopazuch@gmail.com <guilhermopazuch@gmail.com@82bbdaba-fbe1-b293-0f69-b142ebb6a897>
2012-10-14 14:11:39 +0000

    render grid centralized

--- src/level/Level.java
+++ src/level/Level.java
@@ -5,6 +5,8 @@ import utils.CommonResources;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import core.Main;
+
 public class Level {
 	private int rows;
 	private int columns;
@@ -18,6 +20,11 @@ public class Level {
 	private int cell_width = 40;
 	private int cell_height = 40;
 	
+	private int screen_center_x = Main.width / 2;
+	private int screen_center_y = Main.height / 2;
+	private int grid_offset_x = 0;
+	private int grid_offset_y = 0;
+	
 	public Level(int rows, int columns, String name){
 		this.rows = rows;
 		this.columns = columns;
@@ -27,6 +34,10 @@ public class Level {
 		target = null;
 		playerStartX = 0;
 		playerStartY = 0;
+		screen_center_x = Main.width / 2;
+		screen_center_y = Main.height / 2;
+		grid_offset_x = screen_center_x-(cell_width*columns/2);
+		grid_offset_y = screen_center_y-(cell_height*rows/2);
 	}
 
 	public void setBlock(Cell b){
@@ -60,16 +71,21 @@ public class Level {
 
 	public void draw(SpriteBatch spriteBatch){
 		CommonResources res = CommonResources.getInstance();
+		int dest_x = 0;
+		int dest_y = 0;
 		for(int i=0; i < rows; i++){
 			for(int j=0; j< columns; j++){
+//				System.out.println("x: "+i*cell_width+" y: "+j*cell_height);
 				Cell c = grid[i][j];
 				Texture t = res.assets.get(c.tile);
-				spriteBatch.draw(t, i*cell_width, j*cell_height);
+				dest_x = grid_offset_x + (i * (cell_width));
+				dest_y = grid_offset_y + (j * (cell_height));
+				spriteBatch.draw(t, dest_x, dest_y);
 				if(c.isBlockTarget){
-					spriteBatch.draw(res.assets.get(target.tile), i*cell_width, j*cell_height);
+					spriteBatch.draw(res.assets.get(target.tile), dest_x, dest_y);
 				}
 				if(c.hasBlock){
-					spriteBatch.draw(res.assets.get(block.tile), i*cell_width, j*cell_height);
+					spriteBatch.draw(res.assets.get(block.tile), dest_x, dest_y);
 				}
 			}
 		}
